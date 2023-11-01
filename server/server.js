d60110c52ea71bda4e3336a98bea52fad38e6706
Jesse Ilc <jesseilc123@gmail.com>
2023-11-01 17:32:09 -0500

    Working to establish connect with mongoDB, updated server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,22 @@
 const express = require('express')
+const { connectToDb, getDb } = require('./db')
+
+// init app & middleware
 const app = express()
+
+//db connection 
+let db
+
+connectToDb((err) => {
+    if(!err) {
+        app.listen(5555, () => {
+            console.log("listening on port 5555")
+        })
+    }
+    db = getDb()
+})
+
+
 
 app.get("/", (req, res) => {
     console.log('Here')
@@ -10,4 +27,3 @@ const exampleRouter = require('./routes/example')
 
 app.use("/example", exampleRouter)
 
-app.listen(5555)
